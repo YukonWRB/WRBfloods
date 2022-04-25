4eb6268f350408bb4d4c75413634dc87fad1329a
Ghislain.de Laplante <gtdelapl@YNet.gov.yk.ca>
2022-04-25 13:35:48 -0700

    raw data in data-raw, new data file for station info

--- R/data.R
+++ R/data.R
@@ -2,3 +2,8 @@
 #'
 #' @format A data frame containing information on return intervals of water level (2 to 2000 years) for 14 stations in Yukon.
 "return_periods"
+
+#' Table with WSC, ECCC weather reporting stations, and ECCC meteograms coordinates and basic information
+#'
+#' @format A data frame
+"spatial_stns"
