2be877d1e766f0d5ba58e4ce9998162738fc6971
Ghislain.de Laplante <gtdelapl@YNet.gov.yk.ca>
2022-05-13 15:16:21 -0700

    minor changes

--- R/levelPlot.R
+++ R/levelPlot.R
@@ -7,19 +7,16 @@
 #' You must also manually install the dependent package "tidyhydat.ws" as it lives on a github repository. Use install.packages('tidyhydat.ws', repos='https://bcgov.github.io/drat/')
 #'
 #' @param station The WSC station for which you wish to generate a plot.
-#' @param years The year(s) you wish to plot. Maximum of 8 years specified in a vector.
+#' @param years The year(s) you wish to plot. Maximum of 10 years specified in a vector.
 #' @param zoom TRUE/FALSE. If TRUE, the plot x axis (dates) will be truncated to the number of days prior to today specified in zoom_days.
 #' @param zoom_days Number from 1 to 365. Not used unless zoom=TRUE.
 #' @param save_path Where you wish to save the plot. Default is "choose" which brings up the File Explorer for you to choose.
 #'
-#' @return A .png file of the plot requested, plus the plot displayed in RStudio.
+#' @return A .png file of the plot requested. Assign the function to a variable to also get a plot in your global environment.
 #' @export
 #'
 
-
 levelPlot <- function(station, years, zoom=FALSE, zoom_days=30, save_path="choose") {
-  
-  library(tidyhydat.ws) #This is necessary because... because? Problem seems to be with tidyhydat.ws somewhere...
   
   if (save_path == "choose") {
     print("Select the path to the folder where you want this report saved.")
@@ -54,11 +51,10 @@ levelPlot <- function(station, years, zoom=FALSE, zoom_days=30, save_path="choos
     ggplot2::labs(title=paste0("Station ", station, ": ", stringr::str_to_title(tidyhydat::hy_stations(station)[,2]))) +
     ggplot2::theme(plot.title=ggplot2::element_text(hjust=0.05, size=14))
   
-  print(plot)
-  
   #Save it
   ggplot2::ggsave(filename=paste0(save_path,"/", station, "_LEVEL_", if(zoom==TRUE) "ZOOM_" else "", Sys.Date(), "_", lubridate::hour(as.POSIXct(format(Sys.time()), tz='America/Whitehorse')), lubridate::minute(as.POSIXct(format(Sys.time()), tz='America/Whitehorse')), ".png"), plot=plot, height=8, width=12, units="in", device="png", dpi=500)
   
+  return(plot)
 }
   
   
--- R/utils_flow.R
+++ R/utils_flow.R
@@ -33,6 +33,9 @@ utils_flow_data <- function(
 	if (max(select_years) >= lubridate::year(Sys.Date() - 730)) {
 	  token_out <- tidyhydat.ws::token_ws()
 	  
+	  #TODO: remove line below once tidyhydat.ws is fixed.
+	  param_id <- tidyhydat.ws::param_id #This is necessary because data is not stored properly in tidyhydat.ws. Reassess in future to see if param_id is stored in a sysdata.rda file.
+	  
 	  flow_real_time <- tidyhydat.ws::realtime_ws(station_number = station_number, parameters = 47, start_date = ifelse(max(lubridate::year(flow_historic$Date)) == lubridate::year(Sys.Date() - 730), paste(paste(lubridate::year(Sys.Date() - 365)), "01", "01", sep = "-"), paste(paste(lubridate::year(Sys.Date() - 730)), "01", "01", sep = "-")), end_date = ifelse(lubridate::year(Sys.Date()) > max(select_years), paste(max(select_years), "12", "31", sep = "-"), paste(Sys.Date())), token = token_out)
 	  
 	  recent_flow <- data.frame() #creates it in case the if statement below does not run so that the output of the function is constant in class
@@ -159,7 +162,7 @@ utils_daily_flow_plot <- function(
   complete_year,
   plot_years_df,
   dummy_year_df,
-  colours = c("blue", "darkorange", "darkorchid3", "cyan2", "firebrick3", "aquamarine4", "gold1", "chartreuse1", "black"),
+  colours = c("blue", "darkorange", "darkorchid3", "cyan2", "firebrick3", "aquamarine4", "gold1", "chartreuse1", "black", "lightsalmon"),
   legend_position = "right",
   line_size = 1,
   point_size = 0.75
@@ -242,7 +245,7 @@ utils_zoom_flow_plot <- function(
     dummy_year_df,
     zoom_data,
     zoom_days = 30,
-    colours = c("blue", "darkorange", "darkorchid3", "cyan2", "firebrick3", "aquamarine4", "gold1", "chartreuse1", "black"),
+    colours = c("blue", "darkorange", "darkorchid3", "cyan2", "firebrick3", "aquamarine4", "gold1", "chartreuse1", "black", "lightsalmon"),
     legend_position = "right",
     line_size = 1,
     point_size = 0.75
