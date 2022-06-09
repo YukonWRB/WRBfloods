5b76986d82465de8e6dbf4e582533bb9121b0123
Ghislain de Laplante <ghislaindel@gmail.com>
2022-06-09 12:47:52 -0700

    .

--- R/splitMESH.R
+++ R/splitMESH.R
@@ -11,28 +11,28 @@
 #'
 
 splitMESH <- function(file = "choose", output_folder = "choose") {
-
-if (file == "choose"){
-  targetFile <- as.character(utils::choose.files(caption="Select Your File", multi=FALSE, filters=Filters["pdf",]))
-} else targetFile <- file
-
-if (output_folder == "choose") {
-  output <- as.character(utils::choose.dir(caption="Select Save Folder"))
-} else output <- output_folder
   
-pdftext <- pdftools::pdf_text(targetFile)
+  if (file == "choose"){
+    targetFile <- as.character(utils::choose.files(caption="Select Your File", multi=FALSE, filters=Filters["pdf",]))
+  } else targetFile <- file
   
-titles <- stringr::str_extract(pdftext, "[0-9]{2}[A-Za-z]{2}[0-9]{3}") #gets the station codes
+  if (output_folder == "choose") {
+    output <- as.character(utils::choose.dir(caption="Select Save Folder"))
+  } else output <- output_folder
   
-for (i in 1:length(titles)){
-  name <- stringr::str_to_title(tidyhydat::hy_stations(titles[i])$STATION_NAME)
-  name <- gsub("River", "R", name)
-  name <- gsub("Lake", "Lk", name)
-  name <- gsub("Creek", "Ck", name)
-  name <- gsub("Kilometre", "km", name)
-  name <- gsub("Kilometer", "km", name)
-  name <- gsub("Highway", "Hwy", name)
-  qpdf::pdf_subset(targetFile, pages = i, output=paste0(output, "/", titles[i], "_", name, ".pdf"))
-}
+  pdftext <- pdftools::pdf_text(targetFile)
+  
+  titles <- stringr::str_extract(pdftext, "[0-9]{2}[A-Za-z]{2}[0-9]{3}") #gets the station codes
+  
+  for (i in 1:length(titles)){
+    name <- stringr::str_to_title(tidyhydat::hy_stations(titles[i])$STATION_NAME)
+    name <- gsub("River", "R", name)
+    name <- gsub("Lake", "Lk", name)
+    name <- gsub("Creek", "Ck", name)
+    name <- gsub("Kilometre", "km", name)
+    name <- gsub("Kilometer", "km", name)
+    name <- gsub("Highway", "Hwy", name)
+    qpdf::pdf_subset(targetFile, pages = i, output=paste0(output, "/", titles[i], "_", name, ".pdf"))
+  }
   
 } #End of function
