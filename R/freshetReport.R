5a434178fc1407047e9f95c81a8ff349579405de
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2022-06-15 13:29:20 -0700

    Mostly table cell alignments

--- R/freshetReport.R
+++ R/freshetReport.R
@@ -103,7 +103,7 @@ freshetReport <-
         
         rmarkdown::render(
           input = system.file("rmd", "Freshet_report.Rmd", package="WRBfloods"),
-          output_file = paste0(Sys.Date(), "_Yukon-Hydrometric-Conditions-Report", ),
+          output_file = paste0(Sys.Date(), "_Yukon-Hydrometric-Conditions-Report"),
           output_dir = save_path,
           params = list(
             stations = stations,
