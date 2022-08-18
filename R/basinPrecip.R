4c0e66ca2d15ecfec10b9be6d102813c6828a3f7
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2022-08-18 09:10:24 -0700

    Tiny bug fixed with plot levels and CGVD28 levels

---
+++ R/basinPrecip.R
@@ -0,0 +1,7 @@
+#Script to get basin-accumulated precipitation from ECCC GRIB2 rasters and watershed polygons
+
+#Auto-download of GRIB2 raster needs to be set-up on the VM; script will default to those files first, otherwise will download and populate the folder in order to proceed.
+
+#function getHRDPA will be called if the most recent (or other desired time period) raster(s)) are not found.
+
+#Watershed polygons will need to be parsed and saved to sysdata.rda format, or in user-accessible data. (use_data())
