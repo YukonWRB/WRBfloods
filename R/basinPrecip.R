21e5ff5df41041ccf694468125286f320e2e3582
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2022-08-24 12:46:51 -0700

    attempt at getting around relative/absolute file path problem when drives need to be mapped to.

--- R/basinPrecip.R
+++ R/basinPrecip.R
@@ -2,6 +2,9 @@
 
 #Auto-download of GRIB2 raster needs to be set-up on the VM; script will default to those files first, otherwise will download and populate the folder in order to proceed.
 
-#function getHRDPA will be called if the most recent (or other desired time period) raster(s)) are not found.
+#function WRBtools::getHRDPA will be called if the most recent (or other desired time period) raster(s)) are not found. 
 
 #Watershed polygons will need to be parsed and saved to sysdata.rda format, or in user-accessible data. (use_data())
+
+
+#Also explore possibility of asking for precip above certain xy coords
